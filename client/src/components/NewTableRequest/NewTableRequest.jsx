e410a461492864f6a017da8640b8d133d586a47d
Cristhian <cristhian.rabi@alumnos.ucn.cl>
2022-11-04 16:03:05 -0400

    Update NewTableRequest.jsx

--- client/src/components/NewTableRequest/NewTableRequest.jsx
+++ client/src/components/NewTableRequest/NewTableRequest.jsx
@@ -1,35 +1,24 @@
 import React from "react";
 import { useContext } from "react";
+import { createRequest } from "../../api/requests.api.js";
 
 import {BASE_URL} from "../../config.js";
-
 
 const request = {
   "table_id": "M1",
   "status": 1,
   "type": 1,
-  "created_at": "2021-08-01 00:00:00",
-  "updated_at": "2021-08-01 00:00:00"
 };
-
 
 function create(){
   console.log(request);
-  fetch(BASE_URL+"/requests", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(request),
-  }); 
-  
+  createRequest(request);
+
 }
 
 function NewTableRequest() {
 
-
   return (
-
 
     <div className="boton">
       <button onClick={create}>Necesito ayuda!!</button>
