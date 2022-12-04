ae48413de39e24f5292fa8bbbe9af68bb6573f0d
_javieress <javier.rojas04@alumnos.ucn.cl>
2022-12-04 18:32:06 -0300

    Borrar plato
    Se puede borrar plato desde la vista de platos

---
+++ client/src/components/ManageDishes/ItemDish.jsx
@@ -0,0 +1,38 @@
+import { Row, Col } from "reactstrap";
+import { deleteDishRequest } from "../../api/dishes.api";
+
+import "./ItemDish.css";
+
+function ItemDish(params) {
+  function handlerDelete() {
+    // console.log(params.dish.id);
+    deleteDishRequest(params.dish.id).then((response) => {
+      params.setSeed(params.seed + 1);
+    });
+  }
+  return (
+    <div className="dish-item">
+      <Row
+        style={{
+          borderBottom: "1px solid black",
+        }}
+      >
+        <Col className="dish-item-column-name">{params.dish.name}</Col>
+        <Col className="dish-item-column-state">
+          <input
+            type="checkbox"
+            checked={params.dish.is_active}
+            onChange={() => params.changeState(params.dish)}
+          />
+        </Col>
+        <Col className="dish-item-column-delete">
+          <button className="delete-button" onClick={handlerDelete}>
+            Eliminar
+          </button>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default ItemDish;
--- client/src/pages/DishesPage.jsx
+++ client/src/pages/DishesPage.jsx
@@ -1,75 +1,10 @@
-import { Grid } from "@mui/material";
-import { useState, useEffect } from "react";
-import { Card, Row, Col } from "reactstrap";
-import {
-  getDishesRequest,
-  getDishRequest,
-  updateDishRequest,
-} from "../api/dishes.api";
+import Dishes from "../components/ManageDishes/Dishes";
 
 function DishesPage() {
-  const [dishes, setDishes] = useState([]);
-  const [seed, setSeed] = useState(0);
 
-  useEffect(() => {
-    getDishesRequest().then((response) => {
-      setDishes(response.data);
-    });
-  }, []);
-
-  const changeState = (dish) => {
-    dish.is_active = !dish.is_active;
-    updateDishRequest(dish.id, dish).then((response) => {
-      setSeed(seed + 1);
-    });
-  };
 
   return (
-    <div
-      style={{
-        marginTop: "100px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <h1>Platos</h1>
-      <Card
-        style={{
-          padding: "0 10px 10px 10px",
-          margin: "10px ",
-          minWidth: "300px",
-        }}
-      >
-        <Row
-          style={{
-            backgroundColor: "black",
-            color: "white",
-            borderRadius: "10px 10px 0 0",
-          }}
-        >
-          <Col>Plato</Col>
-          <Col>Estado</Col>
-        </Row>
-        {dishes.map((dish, index) => (
-            <Row key={index} 
-            style = {{
-              borderBottom: "1px solid black",
-            }}  
-            >
-              <Col>{dish.name}</Col>
-              <Col>
-                <input
-                  type="checkbox"
-                  checked={dish.is_active}
-                  onChange={() => changeState(dish)}
-                />
-              </Col>
-            </Row>
-        ))}
-      </Card>
-    </div>
+    <Dishes />
   );
 }
 
