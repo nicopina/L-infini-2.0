f012b263d20a6021b2f3b9727b0dc9da6e1df7d5
Piña <64911016+nicopina@users.noreply.github.com>
2022-12-04 16:05:34 -0300

    Contact Mailer Done

---
+++ client/src/components/Mail/ContactForm.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import swal from "sweetalert";
+import { FormControl } from "@mui/material";
+import emailjs from "emailjs-com";
+
+const ContactForm = () => {
+  const formData = (event) => {
+    const { name } = event.target;
+    event.preventDefault();
+
+    emailjs.sendForm("service_4qy0skm", "template_uzrduog", event.target, "Z5o-BzxsYjkBR5fHP").then(
+      (result) => {
+        swal({
+          title: "Gracias por contactarnos " + name.value + "!",
+          text: "Te enviaremos una respuesta en breve al correo que nos has proporcionado.",
+          icon: "success",
+          button: "Aceptar",
+        });
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+  };
+
+  return (
+    <div>
+      <form onSubmit={formData}>
+        <label form="email">
+          <span>Ingresa tu Email</span>
+          <input type="text" name="email" placeholder="tucorreo@example.com" />
+        </label>
+        <label>
+          <span>Ingresa tu nombre</span>
+          <input type="text" name="name" placeholder="Nombre" />
+        </label>
+        <label>
+          <span>Comentanos el motivo de tu contacto</span>
+          <textarea type="text" name="message" placeholder="Mensaje" />
+        </label>
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+};
+export default ContactForm;
