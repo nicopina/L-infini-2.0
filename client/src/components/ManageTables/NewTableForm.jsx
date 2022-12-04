8ab1aa62f2cf7a61ee9a237563ccc3d1d3d80f0f
_javieress <javier.rojas04@alumnos.ucn.cl>
2022-12-04 19:25:13 -0300

    Update Vista Mesas
    Ahora se verifica que se entregue un valor correcto y se mejoró un poco visualmente

--- client/src/components/ManageTables/NewTableForm.jsx
+++ client/src/components/ManageTables/NewTableForm.jsx
@@ -1,25 +1,30 @@
-import {createTableRequest} from '../../api/tables.api';
+import { Card } from "@mui/material";
+import { createTableRequest } from "../../api/tables.api";
+import "./NewTableForm.css";
 
 function NewTableForm(params) {
-
- async function handleAddTable(event) {
+  async function handleAddTable(event) {
     event.preventDefault();
     const newTable = {
-        id: event.target.id.value,
+      id: event.target.id.value,
     };
-   createTableRequest(newTable).then((response) => {
-    params.setSeed(Math.random());
-   });
-}
+    createTableRequest(newTable).then((response) => {
+      params.setSeed(Math.random());
+    });
+  }
 
   return (
-    <form onSubmit={handleAddTable}>
-      <label>
-        ID:
-        <input type="text" name="id" />
-      </label>
-      <button className="button" type="submit">Agrega mesa</button>
-    </form>
+    <Card className="new-table-card">
+      <form onSubmit={handleAddTable} className="new-table-form">
+        <div className="new-table-input-container">
+          <label className="new-table-label">ID</label>
+          <input type="text" name="id" required maxLength={6} />
+        </div>
+        <button className="button" type="submit">
+          Agrega mesa
+        </button>
+      </form>
+    </Card>
   );
 }
 
