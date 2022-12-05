3dd513cfcb66ae3ada999ff46849412a28ab846b
alexi2020 <alexidiaz123alexi@gmail.com>
2022-12-05 19:50:04 -0300

    Vista Mesa Asignada

--- client/src/components/NewAllocatedTable/NewAllocatedTable.jsx
+++ client/src/components/NewAllocatedTable/NewAllocatedTable.jsx
@@ -4,6 +4,9 @@ import { UserContext } from "../../Context/UserContext";
 import {getTableRequest} from "../../api/tables.api";
 import NoTablePage from "../../pages/NoTablePage";
 import './NewAllocatedTable.css';
+import {Card,CardBody} from 'reactstrap';
+import swal from 'sweetalert';
+
   
 
 function NewAllocatedTable() {
@@ -11,6 +14,15 @@ function NewAllocatedTable() {
   const [user, setUser, table, setTable] = useContext(UserContext);
   const [exist, setExist] = useState(false);
 
+ //funcion notificacion swal
+  function notificacion(){
+    swal({
+      title: "Su mesa se asignó con exito",
+      text: "Será redirigido al menú en breve",
+      icon: "info",
+      button: false,
+    });
+  }
  
 
   useEffect(() => {
@@ -26,10 +38,17 @@ function NewAllocatedTable() {
   if (table === undefined) {
     return <NoTablePage />;
   }  
-  
-  return <div className="Message">
-      NewAllocatedTable
-    </div>;
+  setTimeout( function() { window.location.href = "/menu"; }, 3000  )
+  return (
+
+  <Card className="Card">
+    <img src="https://cdn.discordapp.com/attachments/966850902319321183/1048086293642481746/Screenshot_2022-12-02_at_00.53.30-PhotoRoom-2.png" />
+    <CardBody>
+      {notificacion()}
+    </CardBody>
+
+  </Card>
+  )
 }
 
 export default NewAllocatedTable;
