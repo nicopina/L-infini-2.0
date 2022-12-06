8385147f1f1df0ab09b1adbd0554fc18c11842a4
_javieress <javier.rojas04@alumnos.ucn.cl>
2022-12-06 19:09:55 -0300

    Prueba Bug Login

--- client/src/Context/UserContext.jsx
+++ client/src/Context/UserContext.jsx
@@ -1,49 +1,55 @@
-import { createContext , useState , useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
 import { getUserByTokenRequest } from "../api/auth.api";
 
 export const UserContext = createContext();
 export const UserProvider = (props) => {
-    const [user, setUser] = useState({
-        rut: "",
-        name: "",
-        role: localStorage.getItem("role"),
-    }); // inicializador de estado
-    const [table , setTable] = useState(localStorage.getItem("table") || undefined);
-    
-    useEffect(() => {
-        const token = localStorage.getItem("token");
-        if (token !== null) {
-            getUserByTokenRequest(token).then((response) => {
-                if(response.status === 200){    
-                    localStorage.setItem("role", response.data.role);
-                    setUser({
-                        rut: response.data.rut,
-                        name: response.data.name,
-                        role: response.data.role,
-                    });
-                }
-                else {
-                    localStorage.removeItem("token");
-                    localStorage.removeItem("role");
-                    setUser({
-                        rut: "",
-                        name: "",
-                        role: null,
-                    });
-                }
+  const [user, setUser] = useState({
+    rut: "",
+    name: "",
+    role: localStorage.getItem("role"),
+  }); // inicializador de estado
+  const [table, setTable] = useState(
+    localStorage.getItem("table") || undefined
+  );
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token !== null) {
+      try {
+        getUserByTokenRequest(token).then((response) => {
+          if (response.status === 200) {
+            localStorage.setItem("role", response.data.role);
+            setUser({
+              rut: response.data.rut,
+              name: response.data.name,
+              role: response.data.role,
             });
-        }
-    }, []);
+          } else {
+            localStorage.removeItem("token");
+            localStorage.removeItem("role");
+            setUser({
+              rut: "",
+              name: "",
+              role: null,
+            });
+          }
+        });
+      } catch (error) {
+        console.error(error);
+        localStorage.removeItem("token");
+      }
+    }
+  }, []);
 
-    useEffect(() => {
-        if (table !== undefined) {
-            localStorage.setItem("table", table);
-        }
-    }, [table]);
+  useEffect(() => {
+    if (table !== undefined) {
+      localStorage.setItem("table", table);
+    }
+  }, [table]);
 
-    return (
-        <UserContext.Provider value={[user, setUser , table ,setTable]}>
-            {props.children}
-        </UserContext.Provider>
-    );
+  return (
+    <UserContext.Provider value={[user, setUser, table, setTable]}>
+      {props.children}
+    </UserContext.Provider>
+  );
 };
