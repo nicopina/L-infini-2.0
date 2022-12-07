e731b57bc0a24c607321a9f32e42bb96dd655ac6
alexi2020 <alexidiaz123alexi@gmail.com>
2022-12-07 13:55:50 -0300

    Filtros Menu
    Se ha agregado filtros para una mejor experiencia para el usuario

--- client/src/Components/MenuPack/MenuPack.jsx
+++ client/src/Components/MenuPack/MenuPack.jsx
@@ -11,10 +11,24 @@ const MenuPack = () => {
   const value = useContext(DataContext);
 
   const [productos, setProductos] = useState([]);
+  const [filter,setFilter] = useState('Comidas');
+
 
   useEffect(() => {
+    console.log(value.productos[0].filter((item) => item.category === 1));
     setProductos(value.productos[0]);
   }, [value]);
+
+
+  useEffect(() => {
+    if(filter === 'Comidas'){
+      setProductos(value.productos[0].filter((item) => item.category === 1));
+    }else if(filter === 'Bebidas'){
+      setProductos(value.productos[0].filter((item) => item.category === 3));
+    }else if(filter === 'Postres'){
+      setProductos(value.productos[0].filter((item) => item.category === 6));
+    }
+  }, [filter]);
 
   return (
     <div className="menu">
@@ -22,6 +36,12 @@ const MenuPack = () => {
       <Reload />
       <Container>
         <Row>
+          <Col lg="12" className="text-center mb-5"> <h3>Menu Filtro</h3> </Col>
+          <Col lg="12" className="text-center mb-4">
+            <button className="btn" onClick={()=>setFilter('Bebidas')}>Bebidas</button>
+            <button className="btn" onClick={()=>setFilter('Comidas')}>Comidas</button>
+            <button className="btn" onClick={()=>setFilter('Postres')}>Postres</button>
+          </Col>
           {productos.map((item, index) => (
             <Col key={index} lg="3" md="4" sm="6" xs="6" className="mb-4">
               {""}
